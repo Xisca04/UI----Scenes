79a52eec50a475272e83d95d108add56736c284c
Xisca04 <xiscacerdavicens@gmail.com>
2023-04-17 08:50:58 +0200

    persistencia_datos_PlayerPrefs

--- Assets/Scripts/UiManager1.cs
+++ Assets/Scripts/UiManager1.cs
@@ -21,4 +21,25 @@ public class UiManager1 : MonoBehaviour
             DataPersistence.sharedInstance.username = inputText;
         }
     }
+
+    /* Función para la persistencia de datos entre partidas
+     
+    public void SaveUsernameWithPlayerPrefs()
+    {
+        PlayerPrefs.SetString("USERNAME", DataPersistence.sharedInstance.username);
+    }
+    
+    private string existingUsername;
+    private const string USERNAME = "USERNAME"; // tipo CONS no se puede modificar, es una constante
+
+    private void Start()
+    {
+        existingUsername = PlayerPrefs.GetString(USERNAME); // Busca si existe la clave username
+
+        if(existingUsername != "")
+        {
+            inputField.placeholder.GetComponent<TextMeshProUGUI>().text = existingUsername; // Si se ha guardado un username en player prefs, se sobrescribe
+        }
+    }
+    */
 }
